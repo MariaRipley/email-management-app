1f2335a4ed666287e7b9d1bca20614512bd26d43
MariaRipley <roblesb.maria@gmail.com>
2023-08-30 16:41:56 +0200

    Add components

--- 01/src/components/App.js
+++ 01/src/components/App.js
@@ -1,32 +1,10 @@
+import Header from "./Header";
 import "../stylesheets/App.css";
 
 function App() {
   return (
     <div>
-      <header className="col2 mb-1">
-        <div>
-          <h1 className="title">
-            <span className="fas fa-envelope-open-text"></span>
-            Gmail
-          </h1>
-        </div>
-
-        <form className="text-align-right">
-          <button className="form__btn">
-            <span className="fas fa-inbox"></span>
-            Recibidos
-          </button>
-          <button className="form__btn">
-            <span className="fas fa-trash"></span>
-            Papelera
-          </button>
-          <input
-            className="form__input-text"
-            type="text"
-            placeholder="Buscar un correo"
-          />
-        </form>
-      </header>
+      <Header />
 
       <table className="table">
         <tbody>
---
+++ 01/src/components/Header.js
@@ -0,0 +1,19 @@
+import HeaderForm from "./HeaderForm";
+import '../stylesheets/Header.css';
+
+function Header() {
+  return (
+    <header className="col2 mb-1">
+      <div>
+        <h1 className="title">
+          <span className="fas fa-envelope-open-text"></span>
+          Gmail
+        </h1>
+      </div>
+
+      <HeaderForm />
+    </header>
+  );
+}
+
+export default Header;
